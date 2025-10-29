1b4bdc29b5f31cd0802031d0e7cb1903b8139e56
Rui Paolo Antunes Faisal <edomex215@amerike.edu.mx>
2025-10-29 12:30:58 -0600

    commit

--- juego_de_la_vida_Rui_Faisal.c
+++ juego_de_la_vida_Rui_Faisal.c
@@ -1,62 +1,99 @@
-//libreria 
-#include "raylib.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
 
-// **conteo y estado de vecinos**
-
-#define filas 50
-#define columnas 50
-#define Tam_celda 10
+#define filas 10
+#define columnas 10
 
 #define death 0
 #define alive 1
 
-// **funciones auxliares**
-
 int contarvecinos(int grid[filas][columnas], int x, int y) {
     int conteo = 0;
-    for (int i = -1; i <= 1; i++){
-        for (int j= -1; i <= 1; j++){
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
             if (i == 0 && j == 0) continue;
             int nx = x + i;
             int ny = y + j;
             if (nx >= 0 && nx < filas && ny >= 0 && ny < columnas) {
-                conteo +- grid[nx][ny];
+                conteo += grid[nx][ny];
             }
         }
     }
     return conteo;
 }
-// **nacimientos o nuevas generaciones**
 
-void nextgeneration (int grid[filas][columnas]) {
-    int new [filas][columnas];
+void nextgeneration(int grid[filas][columnas]) {
+    int nuevo[filas][columnas];
 
-    for (int i = 0; i< filas;i++) {
-        for (int j = 0; j < columnas; j++);
-        int vecinos = contarvecinos(grid, i, j);
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            int vecinos = contarvecinos(grid, i, j);
 
-        //**reglas del juego de la vida**
-
-        if(grid[i][j] == alive) {
-            //si tiene 2 o 3 vecinos vivos, sobrevive
-            // si tiene >=1 muere o si tiene <=4 muere
-            new[i][j] = (vecinos == 2 || vecinos == 3) ? alive:death;
-        }else{
-            //si esta muerta pero tiene 3 vecinos nace 1+
-            new[i][j] = (vecinos == 3) ? alive:death;
+            if (grid[i][j] == alive) {
+                nuevo[i][j] = (vecinos == 2 || vecinos == 3) ? alive : death;
+            } else {
+                nuevo[i][j] = (vecinos == 3) ? alive : death;
+            }
         }
+    }
 
+    for (int i = 0; i < filas; i++)
+        for (int j = 0; j < columnas; j++)
+            grid[i][j] = nuevo[i][j];
+}
+
+void imprimir(int grid[filas][columnas]) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("%c ", grid[i][j] ? '#' : '.');
+        }
+        printf("\n");
     }
 }
 
-//**Copiamos la nueva generación al tablero principal**
+void limpiarPantalla() {
+    // Limpia la pantalla según el sistema operativo
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
 
-    for (int i = 0; i < FILAS; i++)
-        for (int j = 0; j < COLUMNAS; j++)
-            grid[i][j] = new[i][j];
+void draw(int grid[filas][columnas], int gen) {
+    printf("Generación %d:\n", gen);
+    imprimir(grid);
+    printf("\n");
+}
 
-int main (){
-    
+int main() { 
+    int grid[filas][columnas];
+    srand(time(NULL));
+
+    // Inicialización aleatoria
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            grid[i][j] = rand() % 2;
+        }
+    }
+
+    // Simulación
+    for (int gen = 0; gen < 50; gen++) {
+        draw(grid, gen);
+        nextgeneration(grid);
+
+        #ifdef _WIN32
+            Sleep(300);
+        #else
+            usleep(3000000);
+        #endif
+    }
+
+    return 0;
 }
