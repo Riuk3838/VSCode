61b2ddf11546989f2fde29bbe56154d006223619
Rui Paolo Antunes Faisal <edomex215@amerike.edu.mx>
2025-11-24 11:42:07 -0600

    addition of cat

--- juego_de_la_vida_Rui_Faisal.c
+++ juego_de_la_vida_Rui_Faisal.c
@@ -1,8 +1,8 @@
 #include "raylib.h"
 #include <stdbool.h>
 
-#define MAX_FRAME_SPEED     15
-#define MIN_FRAME_SPEED      1
+#define MAX_FRAME_SPEED 15
+#define MIN_FRAME_SPEED  1
 
 typedef struct Player {
     Rectangle rect;
@@ -17,82 +17,114 @@ int main(void) {
     InitWindow(screenWidth, screenHeight, "Plataformero con Raylib");
     SetTargetFPS(60);
 
-    // Jugador
-    InitWindow(screenWidth,screenHeight,"raylib [textures] example - sprite animation");
-    Texture2D scarfy = LoadTexture("cat_walk_1.png");}
-    Vector2 position = { 350.0f, 280.0f}
-    }
+    // --------------------------------------------------------------------
+    //                       SPRITE DEL JUGADOR
+    // --------------------------------------------------------------------
+    Texture2D texture = LoadTexture("Cat_player_images/Cat_sheets/Cat_walk_1.png");
+    Rectangle frameRec = { 0.0f, 0.0f, (float)texture.width/3, (float)texture.height };
+    int currentFrame = 0;
+    int framesCounter = 0;
+    int framesSpeed = 2;
 
-    // Plataforma principal (suelo)
-    Rectangle floor = {0, 400, 800, 50};
+    // --------------------------------------------------------------------
+    //                        DATOS DEL JUGADOR
+    // --------------------------------------------------------------------
+    Player player = {0};
+    player.rect = (Rectangle){350, 280, frameRec.width, frameRec.height};
+    player.velocity = (Vector2){0, 0};
+    player.onGround = false;
 
-    // Otra plataforma
-    Rectangle platform = {300, 300, 150, 20};
+    // --------------------------------------------------------------------
+    //                     PLATAFORMAS Y FÍSICAS
+    // --------------------------------------------------------------------
+    Rectangle floor = (Rectangle){0, 400, 800, 50};
+    Rectangle platform = (Rectangle){300, 300, 150, 20};
 
-    const float gravity = 600.0f; // gravedad en pixels/segundo^2
+    const float gravity = 600.0f;
     const float speed = 200.0f;
     const float jumpForce = -300.0f;
 
+    // --------------------------------------------------------------------
+    //                         GAME LOOP
+    // --------------------------------------------------------------------
     while (!WindowShouldClose()) {
+
         float dt = GetFrameTime();
 
-        // --------------------------
-        //   INPUT DEL JUGADOR
-        // --------------------------
-        if (IsKeyDown(KEY_RIGHT)) player.velocity.x = speed;
-        else if (IsKeyDown(KEY_LEFT)) player.velocity.x = -speed;
-        else player.velocity.x = 0;
+        // ANIMACIÓN del spritesheet
+        framesCounter++;
+        if (framesCounter >= (60/framesSpeed)) {
+            framesCounter = 0;
+            currentFrame++;
+
+            if (currentFrame > 5) currentFrame = 0;
+
+            frameRec.x = frameRec.width * currentFrame;
+        }
+
+        // Control de velocidad de animación (debug)
+        if (IsKeyPressed(KEY_RIGHT)) framesSpeed++;
+        if (IsKeyPressed(KEY_LEFT)) framesSpeed--;
+        if (framesSpeed > MAX_FRAME_SPEED) framesSpeed = MAX_FRAME_SPEED;
+        if (framesSpeed < MIN_FRAME_SPEED) framesSpeed = MIN_FRAME_SPEED;
+
+        // ---------------------------------------------------------
+        //                INPUT
+        // ---------------------------------------------------------
+        player.velocity.x = 0;
+        if (IsKeyDown(KEY_D)) player.velocity.x = speed;
+        if (IsKeyDown(KEY_A)) player.velocity.x = -speed;
 
         if (IsKeyPressed(KEY_SPACE) && player.onGround) {
             player.velocity.y = jumpForce;
             player.onGround = false;
         }
 
-        // --------------------------
-        //        FÍSICA
-        // --------------------------
+        // ---------------------------------------------------------
+        //                FÍSICAS
+        // ---------------------------------------------------------
         player.velocity.y += gravity * dt;
         player.rect.x += player.velocity.x * dt;
         player.rect.y += player.velocity.y * dt;
 
-        // --------------------------
-        //     COLISIONES
-        // --------------------------
+        // Resetear estado
         player.onGround = false;
 
-        // Suelo
+        // Colisión con suelo
         if (CheckCollisionRecs(player.rect, floor)) {
             player.rect.y = floor.y - player.rect.height;
             player.velocity.y = 0;
             player.onGround = true;
         }
 
-        // Plataforma flotante
+        // Colisión con plataforma
         if (CheckCollisionRecs(player.rect, platform)) {
-            if (player.velocity.y > 0) { // cae desde arriba
+            if (player.velocity.y > 0) {
                 player.rect.y = platform.y - player.rect.height;
                 player.velocity.y = 0;
                 player.onGround = true;
             }
         }
 
-        // --------------------------
-        //     DIBUJO
-        // --------------------------
+        // ---------------------------------------------------------
+        //                DRAW
+        // ---------------------------------------------------------
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         DrawRectangleRec(floor, DARKGRAY);
         DrawRectangleRec(platform, GRAY);
 
-        DrawRectangleRec(player.rect, BLUE);
+        // Dibujar el sprite animado
+        DrawTextureRec(texture, frameRec, (Vector2){player.rect.x, player.rect.y}, WHITE);
 
         DrawText("Plataformero en C + Raylib", 10, 10, 20, BLACK);
-        DrawText("Flechas para moverte, SPACE para saltar", 10, 35, 18, DARKBLUE);
+        DrawText("A/D para moverte, SPACE para saltar", 10, 35, 18, DARKBLUE);
 
         EndDrawing();
     }
 
+    UnloadTexture(texture);
     CloseWindow();
     return 0;
 }
